1edfbf19a84d7eb208629c801b3f5cd1883d653a
unknown <temurbekqurbonov01tuit@gmail.com>
2024-02-25 18:06:26 +0500

    ll

--- keyboards/default/stateKey.py
+++ keyboards/default/stateKey.py
@@ -1,63 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
 
-<<<<<<< HEAD
-=======
-citykey = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="Toshkent")
-        ],
-        [
-            KeyboardButton(text="Bekor qilish")
-        ],
-        
-    ],
-    resize_keyboard=True
-)
-
-
-filialkey = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="Sergile"),
-            KeyboardButton(text="Qoratosh")
-        ],
-                [
-            KeyboardButton(text="Beruniy"),
-            KeyboardButton(text="Yunusobod")
-        ],
-                [
-            KeyboardButton(text="Index"),
-            KeyboardButton(text="Qorasuv")
-        ],
-        [
-            KeyboardButton(text="Bekor qilish")
-        ],
-        
-    ],
-    resize_keyboard=True
-)
-
-positionkey = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="Sotuvchi"),
-            KeyboardButton(text="Kasser")
-        ],
-        
-        [
-            KeyboardButton(text="Qorovul"),
-            KeyboardButton(text="Ish boshqaruvchi")
-        ],
-
-        [
-            KeyboardButton(text="Bekor qilish")
-        ],
-    ],
-    resize_keyboard=True
-)
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
 
 studentkey = ReplyKeyboardMarkup(
     keyboard=[
@@ -66,11 +9,7 @@ studentkey = ReplyKeyboardMarkup(
             KeyboardButton(text="Yo'q"),
         ],
         [
-<<<<<<< HEAD
             KeyboardButton(text="Bosh menu")
-=======
-            KeyboardButton(text="Bekor qilish")
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
         ],
                 
     ],
@@ -84,11 +23,7 @@ sogliqkey = ReplyKeyboardMarkup(
             KeyboardButton(text="Sog'lig'imda muommo bor"),
         ],
         [
-<<<<<<< HEAD
             KeyboardButton(text="Bosh menu")
-=======
-            KeyboardButton(text="Bekor qilish")
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
         ],
                 
     ],
@@ -96,39 +31,13 @@ sogliqkey = ReplyKeyboardMarkup(
 )
 
 
-<<<<<<< HEAD
-=======
-salarykey = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="1.5 mln - 2 mln"),
-            KeyboardButton(text="2.5 mln - 3.5 mln")
-        ],
-        [
-            KeyboardButton(text="4 mln - 5 mln"),
-            KeyboardButton(text="5 mln - 6 mln"),
-        ],
-
-        [
-            KeyboardButton(text="Bekor qilish")
-        ],
-                
-    ],
-    resize_keyboard=True
-)
-
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
 phonkey = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="Telefon nomer",request_contact=True),
         ],
         [
-<<<<<<< HEAD
             KeyboardButton(text="Bosh menu")
-=======
-            KeyboardButton(text="Bekor qilish")
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
         ],
           
     ],
@@ -143,11 +52,7 @@ yuborishkey = ReplyKeyboardMarkup(
             KeyboardButton(text="Rad etish")
         ],
         [
-<<<<<<< HEAD
             KeyboardButton(text="Bosh menu")
-=======
-            KeyboardButton(text="Bekor qilish")
->>>>>>> 76bcd2fcd4b825b10a9835310a7eb6c8307d8811
         ],
           
     ],
